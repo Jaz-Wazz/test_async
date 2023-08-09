ab29753bb0d580690201f1a07e87492a3995a28c
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-08-09 18:11:41 +0300

    Fist work server prototype.

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,13 +2,26 @@
 #include <iofox.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <string_view>
 #include <fmt/core.h>
 #include <exception>
 
 namespace asio { using namespace boost::asio; }
 
-auto coro() -> io::coro<void>
+auto coro(asio::io_context & executor) -> io::coro<void>
 {
+	asio::ip::tcp::endpoint endpoint {asio::ip::make_address("127.0.0.1"), 555};
+	asio::ip::tcp::acceptor acceptor {executor, endpoint};
+	auto socket = co_await acceptor.async_accept(io::use_coro);
+
+	for(;;)
+	{
+		char buffer[1024];
+		auto size = co_await socket.async_receive(asio::buffer(buffer, 1024), io::use_coro);
+		fmt::print("[coro] - receive '{}' bytes. data: '{}'.\n", size, std::string_view(buffer, size));
+	}
+
 	fmt::print("sas.\n");
 	co_return;
 }
@@ -17,7 +30,7 @@ int main() try
 {
 	io::windows::set_asio_locale(io::windows::lang::english);
 	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(), io::rethrowed);
+	asio::co_spawn(ctx, coro(ctx), io::rethrowed);
 	ctx.run();
 	return 0;
 }
