9f3bddf2e43caea2f72fa07a133b4c3024051128
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-08-09 22:38:01 +0300

    Add channels for socket.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,41 +1,50 @@
 
 #include <iofox.hpp>
+#include <boost/asio/experimental/channel.hpp>
 #include <boost/asio/completion_condition.hpp>
 #include <boost/asio/steady_timer.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
+#include <boost/asio/this_coro.hpp>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/write.hpp>
+#include <boost/system/error_code.hpp>
 #include <fmt/core.h>
 #include <exception>
 #include <chrono>
 #include <string>
+#include <vector>
 
-namespace asio { using namespace boost::asio; }
+namespace asio { using namespace boost::asio; using namespace boost::asio::experimental; }
 
-auto writer(char name, asio::io_context & executor, asio::ip::tcp::socket & socket) -> io::coro<void>
+auto reader(asio::io_context & executor, asio::channel<void(boost::system::error_code, std::string)> & channel) -> io::coro<void>
 {
+	asio::ip::tcp::socket socket {executor};
+	asio::ip::tcp::endpoint endpoint {asio::ip::make_address("127.0.0.1"), 555};
+	co_await socket.async_connect(endpoint, io::use_coro);
+
 	for(;;)
 	{
-		std::string buffer(100000, name);
+		auto buffer = co_await channel.async_receive(io::use_coro);
 
 		int package_size = buffer.size();
 		buffer.insert(0, reinterpret_cast<char *>(&package_size), 4);
 
 		co_await asio::async_write(socket, asio::buffer(buffer), asio::transfer_exactly(buffer.size()), io::use_coro);
-		fmt::print("[coro '{}'] - send '{}' bytes. data: '{}'.\n", name, buffer.size() - 4, buffer.substr(4));
+		fmt::print("[reader] - read {} bytes and send to server.\n", buffer.size() - 4);
 	}
 }
 
-auto coro(asio::io_context & executor, asio::ip::tcp::socket & socket) -> io::coro<void>
+auto writer(char name, std::string str, asio::channel<void(boost::system::error_code, std::string)> & channel) -> io::coro<void>
 {
-	asio::ip::tcp::endpoint endpoint {asio::ip::make_address("127.0.0.1"), 555};
-	co_await socket.async_connect(endpoint, io::use_coro);
-
-	asio::co_spawn(executor, writer('x', executor, socket), io::rethrowed);
-	asio::co_spawn(executor, writer('y', executor, socket), io::rethrowed);
+	for(;;)
+	{
+		fmt::print("[writer '{}'] - write: '{}'.\n", name, str);
+		co_await channel.async_send({}, str, io::use_coro);
+		co_await asio::steady_timer(co_await asio::this_coro::executor, std::chrono::seconds(1)).async_wait(io::use_coro);
+	}
 }
 
 int main() try
@@ -43,8 +52,10 @@ int main() try
 	io::windows::set_asio_locale(io::windows::lang::english);
 	asio::io_context ctx;
 
-	asio::ip::tcp::socket socket {ctx};
-	asio::co_spawn(ctx, coro(ctx, socket), io::rethrowed);
+	asio::channel<void(boost::system::error_code, std::string)> channel {ctx};
+	asio::co_spawn(ctx, reader(ctx, channel), io::rethrowed);
+	asio::co_spawn(ctx, writer('x', "garox", channel), io::rethrowed);
+	asio::co_spawn(ctx, writer('y', "dudos", channel), io::rethrowed);
 
 	ctx.run();
 	return 0;
