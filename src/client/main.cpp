318bd6c2acaf3995fd6bc0c12b5461393d2e452d
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-08-09 18:32:15 +0300

    Add client multicoro.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -13,26 +13,34 @@
 
 namespace asio { using namespace boost::asio; }
 
-auto coro(asio::io_context & executor) -> io::coro<void>
+auto writer(char name, asio::io_context & executor, asio::ip::tcp::socket & socket) -> io::coro<void>
 {
-	asio::ip::tcp::socket socket {executor};
+	for(;;)
+	{
+		std::string buffer = (name == 'x') ? "garox" : "dudos";
+		auto size = co_await socket.async_write_some(asio::buffer(buffer), io::use_coro);
+		fmt::print("[coro '{}'] - send '{}' bytes. data: '{}'.\n", name, size, buffer);
+		co_await asio::steady_timer(executor, std::chrono::seconds(1)).async_wait(io::use_coro);
+	}
+}
+
+auto coro(asio::io_context & executor, asio::ip::tcp::socket & socket) -> io::coro<void>
+{
 	asio::ip::tcp::endpoint endpoint {asio::ip::make_address("127.0.0.1"), 555};
 	co_await socket.async_connect(endpoint, io::use_coro);
 
-	for(;;)
-	{
-		std::string buffer = "garox";
-		auto size = co_await socket.async_write_some(asio::buffer(buffer), io::use_coro);
-		fmt::print("[coro] - send '{}' bytes. data: '{}'.\n", size, buffer);
-		co_await asio::steady_timer(executor, std::chrono::seconds(1)).async_wait(io::use_coro);
-	}
+	asio::co_spawn(executor, writer('x', executor, socket), io::rethrowed);
+	asio::co_spawn(executor, writer('y', executor, socket), io::rethrowed);
 }
 
 int main() try
 {
 	io::windows::set_asio_locale(io::windows::lang::english);
 	asio::io_context ctx;
-	asio::co_spawn(ctx, coro(ctx), io::rethrowed);
+
+	asio::ip::tcp::socket socket {ctx};
+	asio::co_spawn(ctx, coro(ctx, socket), io::rethrowed);
+
 	ctx.run();
 	return 0;
 }
